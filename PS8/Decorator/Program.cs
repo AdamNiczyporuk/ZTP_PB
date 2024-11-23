afd8457e65cffc0dbb419c793dda84288c3a6408
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-23 09:16:16 +0100

    Add clas Message

---
+++ PS8/Decorator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public Message(string title, string content)
+    {
+        Title = title;
+        Content = content;
+    }
+}
