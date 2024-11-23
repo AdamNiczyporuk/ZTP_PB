2c4c46e88c4e5d4dcb8506cf2ebdfcec372ef527
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-23 09:16:31 +0100

    Add MEssageBOX

--- PS8/Decorator/Program.cs
+++ PS8/Decorator/Program.cs
@@ -13,3 +13,36 @@ public class Message
         Content = content;
     }
 }
+
+public class MessageBox
+{
+    private List<Message> messages = new List<Message>();
+    private int nextId = 1;
+
+    public void AddMessage(Message message)
+    {
+        message.Id = nextId++;
+        messages.Add(message);
+    }
+
+    public Message GetMessageById(int id)
+    {
+        return messages.Find(m => m.Id == id);
+    }
+
+    public void DisplayAllMessageTitles()
+    {
+        Console.WriteLine("\nLista wiadomości:");
+        if (messages.Count == 0)
+        {
+            Console.WriteLine("Brak wiadomości w skrzynce.");
+        }
+        else
+        {
+            foreach (var message in messages)
+            {
+                Console.WriteLine($"ID: {message.Id} - {message.Title}");
+            }
+        }
+    }
+}
